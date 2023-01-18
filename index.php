15ccfce357830e13a4448a93014228ad9d1fcfd8
junaid604 <93976012+junaid604@users.noreply.github.com>
2023-01-18 17:01:59 +0500

    Update index.php

--- index.php
+++ index.php
@@ -8,62 +8,42 @@
     <script src="js/select2.min.js"></script>
 </head>
 <body>
-    <div class="container">
-  		<br />
-  		<br />
-    	<h1 align="center">Dynamically Add New Option Tag in Select2 using Ajax in PHP</h1>
-    	<br />
-    	<br />	
-      <div class="row">
-        <div class="col-md-6 offset-md-3">
-          <select name="category" id="category" class="form-control form-control-lg select2" multiple>
-            <?php
-            foreach($result as $row)
-            {
-              echo '<option value="'.$row['category_name'].'">'.$row['category_name'].'</option>';
-            }
-            ?>
-          </select>
+    <div class="row mt-5">
+        <div class="col-md-6 offset-3 mt-5">
+            <div class="card">
+                <div class="card-header bg-info text-center text-white">
+                    <h6 class="m-0">PHP Mysql Select2 Autocomplete With Ajax Example - MyWebtuts.com</h6>
+                </div>
+                <div class="card-body" style="height: 280px;">
+                    <div>
+                        <select class="postName form-control" name="postName" multiple></select>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div> 
     </div>
-  </body>
-</html>
-
-<script>
-
-$(document).ready(function(){
-
-  $('.select2').select2({
-    placeholder:'Select Category',
-    theme:'bootstrap4',
-    tags:true,
-  }).on('select2:close', function(){
-    var element = $(this);
-    var new_category = $.trim(element.val());
-
-    if(new_category != '')
-    {
-      $.ajax({
-        url:"add.php",
-        method:"POST",
-        data:{category_name:new_category},
-        success:function(data)
-        {
-          if(data == 'yes')
-          {
-            element.append('<option value="'+new_category+'">'+new_category+'</option>').val(new_category);
-          }
-        }
-      })
-    }
-
-  });
-
-});
-
-</script>
-
+    
+    <script type="text/javascript">
+        $('.postName').select2({
+            placeholder: 'Select a post',
+            ajax: {
+                url: 'fetch.php',
+                dataType: 'json',
+                delay: 250,
+                data: function (data) {
+                    return {
+                        searchTerm: data.term // search term
+                    };
+                },
+                processResults: function (response) {
+                    return {
+                        results:response
+                    };
+                },
+                cache: true
+            }
+        });
+    </script>
     
 </body>
 </html>
